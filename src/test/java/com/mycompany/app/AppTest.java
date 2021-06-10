3300e6111d14fd9fcde9a78aeab2469cf140b952
tranthang2404 <43313369+tranthang2404@users.noreply.github.com>
2021-06-10 22:19:39 +0700

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest
     public void testAppMain() throws Exception {
         App.MyHandler mh = new App.MyHandler();
         try {
-            assertEquals("Hello22", mh.messResponse());
+            assertEquals("Hello", mh.messResponse());
         } catch (AssertionError e) {
             fail("function respone  not true");
         }
